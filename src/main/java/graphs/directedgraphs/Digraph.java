78ff30ebeda04ca87514f0ab60fc2fdd18a2ad26
zheng <765324639@qq.com>
2017-10-27 15:19:53 +0800

    有向图深度优先搜索

---
+++ src/main/java/graphs/directedgraphs/Digraph.java
@@ -0,0 +1,58 @@
+package graphs.directedgraphs;
+
+import graphs.undirectedgraphs.Bag;
+
+public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public Digraph reverse() {
+        Digraph r = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                r.addEdge(w, v);
+            }
+        }
+        return r;
+    }
+
+    @Override
+    public String toString() {
+        String s = V + " vertices, " + E + " edges\n";
+        for (int v = 0; v < V; v++) {
+            s += v + ": ";
+            for (int w : this.adj(v)) {
+                s += w + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+}
